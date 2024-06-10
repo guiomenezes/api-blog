6cb457f9dd9163af5b0650691174c0d2c2e9a697
guiomenezes <guilherme.menezes@outlook.com>
2024-06-10 10:21:33 -0300

    Second change

--- API_SQL.py
+++ API_SQL.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 #Criar uma instância de SQL ALchemy
 app.config['SECRET_KEY'] = 'FSD2323f#$!SAH'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres.hznbnzvpvjdhyukiltkr:Arievilo524@2209%@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.hznbnzvpvjdhyukiltkr:GS48QXj8BkE7esNH@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
 
 db = SQLAlchemy(app)
 db: SQLAlchemy
